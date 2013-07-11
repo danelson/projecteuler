b29e6de1e775204bdad30890c17854c2a70a6e8a
Dan Nelson <danelson88@gmail.com>
2013-07-11 17:01:12 -0500

    initial project version

--- pe027.py
+++ pe027.py
@@ -22,7 +22,7 @@ Find the product of the coefficients, a and b, for the quadratic expression
 that produces the maximum number of primes for consecutive values of n,
 starting with n = 0.
 
-Answer
+Answer = -59231
 '''
 
 import time
